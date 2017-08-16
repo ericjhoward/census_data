f393d00d9630bec6dcbeb4f992831d67850130dd
Eric Howard <howard@nlc.org>
2017-08-16 17:29:58 -0400

    Working model for test_find_place_data.py

--- test_find_place_data.py
+++ test_find_place_data.py
@@ -7,7 +7,21 @@ print("Getting started!")
 #Path to test workbox
 print("Loading in file")
 fname = 'C:\scratch\Recession Drops - FINAL TARGET LIST.xlsx'
-tablelist = ['','','']
+tablelist = ['B01001_001E',
+                'B01001_002E',
+#                'B01001_003E',
+#                'B01001_004E',
+#                'B01001_005E',
+#                'B01001_006E',
+#                'B01001_007E',
+#                'B01001_008E',
+#                'B01001_009E',
+#                'B01001_010E',
+#                'B01001_011E',
+#                'B01001_012E',
+#                'B01001_013E',
+#                'B01001_014E',
+                'B01001_015E']
 #wb = load_workbook(filename=fname)
 #ws = wb['Targets']
 
@@ -18,7 +32,7 @@ def df_get_acs_place_data(df, state_index, place_index, tablenumber, apikey, ver
     addcolumn = []
     nrows = len(df.index)
     i = 0
-    for row in df:
+    for index, row in df.iterrows():
         if verbose:
             print "Processing place %s of %s" %(str(i), str(nrows))
         try:
@@ -38,7 +52,14 @@ def df_get_acs_place_data(df, state_index, place_index, tablenumber, apikey, ver
     df[tablenumber] = pandas.Series(addcolumn, dtype=int)
     return(df)
 
-test = df_get_acs_place_data(df, 'State', 'FIPS Code', 'B06011_001E', apikey, True)
+for table in tablelist:
+    print "Working on %s" % table
+    output = df_get_acs_place_data(df, 'State', 'FIPS Code', table, apikey, True)
+    df = output
+
+
+
+#test = df_get_acs_place_data(df, 'State', 'FIPS Code', 'B06011_001E', apikey, True)
 
 '''
 for row in dataset:
